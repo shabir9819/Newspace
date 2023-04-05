214aae8027dd7dbee3af581667189aa22ed8d078
shabir <shaikhshabir9819@gmail.com>
2023-04-05 21:30:16 +0530

    fourth commit

---
+++ components/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar"
+import Pagination from "@/components/Pagination/Pagination"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { apiData } from "@/config/text";
+
+export default function Home() {
+    const [data, setData] = useState("") ;
+    const recordsPerPage = 6;
+
+    const fetchData = async () => {
+      try {
+        // const options = {
+        //   method: "GET",
+        //   url: "https://newsapi.org/v2/top-headlines?country=in&apiKey=4cc17d846e224155b46bbe916654df47",
+          
+        // };
+    
+        // const response =  await axios.request(options);
+        // console.log(response.data)
+        
+        setData(apiData);
+      
+     
+      } catch (error) {
+        console.log(error);
+      }
+  
+      
+    };
+  
+    useEffect(()=>{
+      fetchData();
+    },[])
+    return (
+      <>
+      <Navbar/>
+     {(data !== "") && <Pagination data ={data} setData= {setData} recordsPerPage={recordsPerPage}/>}
+      </>
+    )
+  }
+
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,6 @@
-import React from "react";
-import Navbar from "@/components/Navbar/Navbar"
-import Pagination from "@/components/Pagination/Pagination"
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { apiData } from "@/config/text";
-
+import Home from "@/components/Home/Home";
 
 function index() {
-  const [data, setData] = useState("") ;
- 
-  
-  const recordsPerPage = 6;
-
-  const fetchData = async () => {
-    try {
-      // const options = {
-      //   method: "GET",
-      //   url: "https://newsapi.org/v2/top-headlines?country=in&apiKey=4cc17d846e224155b46bbe916654df47",
-        
-      // };
-  
-      // const response =  await axios.request(options);
-      // console.log(response.data)
-      
-      setData(apiData);
-    
-   
-    } catch (error) {
-      console.log(error);
-    }
-
-    
-  };
-
-  useEffect(()=>{
-    fetchData();
-  },[])
-  return (
-    <>
-    <Navbar/>
-   {(data !== "") && <Pagination data ={data} setData= {setData} recordsPerPage={recordsPerPage}/>}
-    </>
-  )
+  return <Home />;
 }
-
-export default index
+export default index;
